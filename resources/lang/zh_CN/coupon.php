a9c8d79f38eb461ad4c7793dc6108113b89c07e7
FuaerCN <FuaerCN@Gmail.com>
2026-03-05 10:23:34 +0800

    新增优惠方式：系数折扣

--- resources/lang/zh_CN/coupon.php
+++ resources/lang/zh_CN/coupon.php
@@ -7,17 +7,19 @@ return [
     ],
     'fields' => [
         'type' => '优惠类型',
-        'discount' => '优惠金额',
-        'is_use' => '是否已经使用',
+        'discount' => '优惠金额/系数',
         'is_open' => '是否启用',
         'coupon' => '优惠码',
         'ret' => '剩余使用次数',
         'type_one_time' => '一次性使用',
         'type_repeat' => '重复使用',
-        'status_use' => '已使用',
-        'status_unused' => '未使用',
+        'type_percent' => '系数折扣 (0-1)',
+        'type_fixed' => '固定金额',
         'goods_id' => '可用商品'
     ],
     'options' => [
     ],
+    'helps' =>[
+        'discount' => '系数折扣：价格在下单时会乘以这个数字，0.9就是九折。固定金额：下单时价格会直接减去它。'
+    ]
 ];
--- resources/lang/zh_TW/coupon.php
+++ resources/lang/zh_TW/coupon.php
@@ -7,17 +7,19 @@ return [
     ],
     'fields' => [
         'type' => '折扣類型',
-        'discount' => '折扣金額',
-        'is_use' => '是否已經使用',
+        'discount' => '折扣金額/系数',
         'is_open' => '是否啟用',
         'coupon' => '折扣碼',
         'ret' => '剩余使用次數',
         'type_one_time' => '一次性使用',
         'type_repeat' => '重復使用',
-        'status_use' => '已使用',
-        'status_unused' => '未使用',
+        'type_percent' => '係數折扣 (0-1)',
+        'type_fixed' => '固定金額',
         'goods_id' => '可用商品'
     ],
     'options' => [
     ],
+    'helps' =>[
+        'discount' => '系数折扣：价格在下单时会乘以这个数字，0.9就是九折。固定金额：下单时价格会直接减去它。'
+    ]
 ];
