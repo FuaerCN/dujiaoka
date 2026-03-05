8eaeb2b6c34b89360276ede1f570f6e93031e9de
FuaerCN <FuaerCN@Gmail.com>
2026-03-05 10:24:48 +0800

    新增支付通道费率设置

--- resources/lang/zh_TW/pay.php
+++ resources/lang/zh_TW/pay.php
@@ -11,6 +11,7 @@ return [
         'merchant_pem' => '商戶金鑰',
         'pay_check' => '支付標識',
         'pay_client' => '支付場景',
+        'pay_fee' => '通道費率',
         'pay_handleroute' => '支付處理路由',
         'pay_method' => '支付方式',
         'pay_name' => '支付名稱',
@@ -23,4 +24,7 @@ return [
     ],
     'options' => [
     ],
+    'helps' =>[
+        'pay_fee' => '單位百分比。如填寫0.38，代表0.38%的通道手續費，下單時價格會自動加上這筆手續費。'
+    ]
 ];
